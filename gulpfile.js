a6e221802836753ba0fbc3443e0ffe5aee1d8475
Stan Shvaika <stan@shvaika.com>
2018-08-16 17:57:06 +0300

    Initial release

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+const sourcemaps = require("gulp-sourcemaps");
+const babel = require('gulp-babel');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const pump = require('pump');
+const gulputil = require('gulp-util');
+const pug = require('gulp-pug');
+const prettify = require('gulp-html-prettify');
+const plumber = require('gulp-plumber');
+const beautify = require('gulp-beautify');
+const imagemin = require('gulp-imagemin');
+
+// PostCSS plugins
+var plugins = [
+  autoprefixer({
+    browsers: ['last 2 versions']
+  }),
+  cssnano()
+];
+
+// Scripts
+const scriptsOrder = [
+  'source/js/libs/progressbar.min.js',
+  'source/js/app.js'
+];
+
+// Javascripts
+gulp.task('js', function(cb) {
+  return gulp.src(scriptsOrder)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./docs/assets/js/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+
+// Stylesheets
+gulp.task('sass', function() {
+  return gulp.src('./source/sass/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./docs/assets/css/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+// HTML
+gulp.task('html', function() {
+  return gulp.src('./source/pug/*.pug')
+    .pipe(plumber())
+    .pipe(pug())
+    .pipe(gulp.dest('./docs/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+// BrowserSync
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: "docs"
+  });
+});
+
+// Images
+gulp.task('images', function() {
+  gulp.src('source/images/**/*')
+    .pipe(imagemin({
+      interlaced: true,
+      progressive: true,
+      optimizationLevel: 7
+    }))
+    .pipe(gulp.dest('docs/assets/images'))
+});
+
+// Build
+gulp.task('build', ['js', 'sass', 'html']);
+
+// Default
+gulp.task('default', ['sass', 'js', 'browser-sync'], function() {
+  gulp.watch('./source/sass/**/*.scss', ['sass']);
+  gulp.watch('./source/js/app.js', ['js']);
+  gulp.watch('./source/pug/**/*.pug', ['html']);
+});
---
+++ source/js/app.js
@@ -0,0 +1,302 @@
+;(function($) {
+
+  // parameters and selectors
+  const peopleContainer = $('.js-container');
+  const progressBar = $('.game__progress');
+  const countdownCircle = $('.js-countdown');
+  const countdownElement = $('.js-countdown-text');
+  const roundNumber = $('.js-round');
+  const attempts = $('.js-attempts');
+  const attemptsLabel = $('.js-attempts-label');
+  const resultFalse = $('.game__result_false');
+  const resultTrue = $('.game__result_true');
+  const skipBtn = $('.js-skip');
+
+  const peopleLimit = 8;
+  const totalRounds = 4;
+  const attemptsNumber = 3;
+
+  /**
+   * Generates the random number.
+   *
+   * @param  {number} - Min number.
+   * @param  {number} - Max number.
+   * @return {number} - The random number.
+   */
+  const randomNumber = (min, max) => {
+    let rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+  }
+
+  /**
+   * Capitalises first letter.
+   *
+   * @param  {string} - The words.
+   * @return {string} - The word with a capital letter.
+   */
+  const capitalizeFirstLetter = string => {
+    return string.replace(/\b./g,
+      function(l) {
+        return l.toUpperCase();
+      });
+  }
+
+  /**
+   * Animates circle countdown.
+   *
+   * @param  {number} - Time in seconds.
+   */
+  const animateCircleCountdown = time => {
+
+    // clear
+    countdownCircle.empty();
+    countdownElement.empty();
+
+    let timeLeft = time;
+
+    // text counter
+    const countdown = () => {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+      } else {
+        countdownElement.text(timeLeft);
+        timeLeft--;
+      }
+    }
+
+    // run the first iteration (without delay) and then the rest
+    //     clearInterval(timerId);
+    countdown();
+    let timerId = setInterval(countdown, 1000);
+
+    // use library to animate circle
+    const circle = new ProgressBar.Circle('.js-countdown', {
+      color: '#ffffff',
+      trailColor: '#ff567b',
+      duration: time * 1000,
+      easing: 'linear',
+      strokeWidth: 5,
+      trailWidth: 5
+    });
+    circle.animate(1);
+    return timerId;
+  }
+
+  /**
+   * Renders people.
+   *
+   * @param  {array} - An array with data.
+   */
+  const renderPeople = data => {
+    let i = 1;
+    for (let item of data) {
+      // create full name
+      let fullName = capitalizeFirstLetter(item.name.first) + ' ' + capitalizeFirstLetter(item.name.last);
+
+      // our template
+      let template = `<div class="item d-inline-block" data-id="${i}">
+              <p class="d-none">2</p>
+              <img src="${item.picture.large}" alt="${fullName}">
+              <div class="price">0,3 ФМ</div>
+            </div>`;
+
+      // insert people
+      peopleContainer.append(template);
+
+      i++;
+    }
+  }
+
+  /**
+   * Represents a game.
+   * @constructor
+   * @param {number} round - The round of the game.
+   */
+  class Game {
+
+    /**
+     * Constructor.
+     *
+     * @param  {number} round - Rounds of the game.
+     * @param  {number} attemptsNumber - How many times can user go wrong.
+     */
+    constructor(round, attemptsNumber) {
+      this.round = round;
+      this.attemptsNumber = attemptsNumber;
+      this.answer;
+    }
+
+    /**
+     * Starts round, set the correct answer.
+     */
+    startRound() {
+      // generate and set the answer, only for demo!
+      this.answer = randomNumber(1, 8);
+
+      console.log(`Starting ${this.round} round, the answer is ${this.answer}.`)
+
+      // update text
+      roundNumber.text(this.round);
+
+      // clear
+      peopleContainer.empty();
+
+      // get people
+      $.ajax({
+        url: 'https://randomuser.me/api/?results=8',
+        dataType: 'json',
+        success: (data) => {
+          // render
+          renderPeople(data.results);
+        },
+        error: (xhr, textStatus, error) => {
+          console.log(xhr.responseText);
+        }
+      });
+
+    }
+
+    /**
+     * Ends round, updates the progress bar.
+     */
+    endRound() {
+      console.log(`Round ${this.round} completed.`)
+
+      // update progress bar
+      let point = this.round - 1;
+      if (point >= 0) {
+        // calc width for the bar
+        let width = Math.round(100 / (totalRounds - 1) * point);
+        progressBar.find('.active-line').animate({
+          width: width + '%',
+        }, 300, function() {
+          progressBar.find('.point:eq(' + point + ')').addClass('active');
+        });
+      }
+
+    }
+
+    /**
+     * Resets attempts counter.
+     */
+    resetCounter(intervalId) {
+      clearInterval(intervalId);
+      this.attemptsNumber = 3;
+      attempts.text(3);
+      resultFalse.fadeOut('fast');
+      peopleContainer.removeClass('false');
+    }
+
+    /**
+     * Checks the answer and returs true/false.
+     */
+    checkAnswer(answer) {
+      // give correct person
+      let person = peopleContainer.find('.item[data-id="' + answer + '"]');
+
+      // show messages
+      if (answer === this.answer) {
+        // update image
+        let image = person.find('img').attr('src');
+        resultTrue.find('img').attr('src', image);
+
+        // show message
+        resultTrue.fadeIn('fast');
+
+        // and then hide
+        setTimeout(() => {
+          resultTrue.fadeOut('fast');
+        }, 1000);
+        return true;
+      } else {
+        // disable "wrong" person
+        person.addClass('disabled');
+
+        // decrease and update counter
+        this.attemptsNumber--;
+        attempts.text(this.attemptsNumber);
+        if (this.attemptsNumber == 1) {
+          attemptsLabel.text('ошибка');
+        } else if (this.attemptsNumber == 0) {
+          attemptsLabel.text('ошибок');
+        } else {
+          attemptsLabel.text('ошибки');
+        }
+
+        // update counter if zero
+        if (this.attemptsNumber == 0) {
+          // start 10 sec timer
+          const intervalId = animateCircleCountdown(10);
+          resultFalse.fadeIn('fast');
+          peopleContainer.addClass('false');
+
+          // clear timer and counter
+          const falseTimes = setTimeout(() => {
+            this.resetCounter(intervalId);
+          }, 10000);
+
+          // skip button
+          skipBtn.click(() => {
+            clearTimeout(falseTimes);
+            this.resetCounter(intervalId);
+          });
+        }
+        return false;
+      }
+    }
+
+    /**
+     * Ends the game
+     */
+    endGame() {
+      alert('The end.');
+    }
+
+    set Round(value) {
+      this.round = value;
+    }
+
+    get Round() {
+      return this.round;
+    }
+
+  }
+
+  /**
+   * Run the code when page loaded
+   */
+  $(window).on('load', function() {
+
+    // initialize the game
+    const game = new Game(1, attemptsNumber);
+
+    // start the first round
+    game.startRound();
+
+    // click on person
+    peopleContainer.on('click', '.item:not(.disabled)', function() {
+      let answer = $(this).data('id');
+
+      // check answer and end round if true
+      if (game.checkAnswer(answer) === true) {
+        // end the current round
+        game.endRound();
+
+        // next round
+        if (game.Round < totalRounds) {
+          game.Round += 1;
+          game.startRound();
+        } else {
+          // or end the game
+          setTimeout(() => {
+            game.endGame();
+          }, 500);
+        }
+      }
+
+    });
+
+  });
+
+})($);
